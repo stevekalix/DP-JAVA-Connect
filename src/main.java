55711cfac20bb31202ecb7fd5a8ed9b7245cc2a0
stevekalix <12345678+stevekalix@users.noreply.github.com>
2025-05-15 11:43:10 +0530

    second commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+class main(){
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+           System.out.println("i AM MANIKANDAN");
+              System.out.println("Hello THAT IS WORLD");
+                 System.out.println("Hello, World!");
+    }
+}
