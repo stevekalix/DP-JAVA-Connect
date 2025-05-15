473d6ac8c5bcdea8d2cf97e1cb793561362b9e69
stevekalix <12345678+stevekalix@users.noreply.github.com>
2025-05-15 10:55:59 +0530

    first commit

---
+++ src/sample/sample.java
@@ -0,0 +1,12 @@
+package sample;
+import java.util.*;
+public class sample {
+
+	public static void main(String[] args) {
+	 double num=10217847;
+	 System.out.printf("%.2f",num);
+	 System.out.print("manikandan iyyappan samiye saranam iyyappa");
+
+	}
+
+}
